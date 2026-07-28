8e5c2530603e4513509b7e530cd8f6560017db6e
Dipto Paul <diptopaul.contact@gmail.com>
2026-07-29 00:11:45 +0600

    added NavigationBar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,8 +16,20 @@ class HomePage extends StatelessWidget {
         title: Text("Navigation and Routing", style: TextStyle(fontWeight: FontWeight.bold, )),
         centerTitle: true,
         backgroundColor: Colors.blueAccent,
-
+        elevation: 1,
       ),
+      body: Center(
+        child: Text("This is Home Screen"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.purpleAccent,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white54,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings"),
+
+      ]),
 
     );
   }
